fc047d9274006e35a21cd6ec9281a3a8ff6626e7
Jeff Hansen <jeffxhansen@gmail.com>
2025-12-11 21:24:20 -0700

    syncing

---
+++ src/open.py
@@ -0,0 +1,20 @@
+from gpiozero import Servo, Device
+from gpiozero.pins.lgpio import LGPIOFactory
+from time import sleep
+
+# Use lgpio pin factory for better PWM control
+Device.pin_factory = LGPIOFactory()
+
+GPIO_PIN = 18
+
+servo = Servo(GPIO_PIN)
+
+try:
+    
+    max_up_position = -0.4
+    
+    servo.value = max_up_position
+    sleep(0.2)
+    
+finally:
+    servo.close()
